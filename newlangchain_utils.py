e60bd982264c85e34ec99ed6eabe8b9530a095ab
KrushnaBaral <krushna.baral@lagozon.com>
2025-12-18 14:27:17 +0530

    Update newlangchain_utils.py

--- newlangchain_utils.py
+++ newlangchain_utils.py
@@ -84,7 +84,7 @@ import configure
 # db_database=os.getenv("db_database")
 # db_port=os.getenv("db_port")
 # db_schema= os.getenv("db_schema")
-db_tables = os.getenv("DB_TABLES").split(",")
+db_tables =  json.loads(os.getenv("DB_TABLES"))
 
 
 SQL_DB_SERVER = os.getenv("SQL_DB_SERVER")
@@ -619,5 +619,6 @@ def find_relationships_for_tables(table_names, json_file_path):
         all_related[table_name] = related
 
     return all_related
+
 
 
